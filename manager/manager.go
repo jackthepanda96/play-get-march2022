7d81adf87739d48f1c7d21a3baa432dbd9770f88
Jerry Young <jerryyoung2911@gmail.com>
2022-03-22 15:00:35 +0700

    Tambah fitur hukum rekan

--- manager/manager.go
+++ manager/manager.go
@@ -2,6 +2,10 @@ package manager
 
 import "fmt"
 
+func HukumRekanKerja() {
+	fmt.Println("Ayok kerja!! BELUM TIPES!!!")
+}
+
 func AssignKerjaan() {
 	fmt.Println("Mengarahkan pekerjaan pada si X")
 }
